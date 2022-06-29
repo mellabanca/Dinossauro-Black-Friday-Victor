44bee44c9b187213542d431eb3cb8e81f728f653
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-06-29 15:34:48 -0300

    29/06/2022

--- sketch.js
+++ sketch.js
@@ -1,41 +1,45 @@
 //Criando as variáveis
 var dinossauroCorrendoNaBlackFriday;
 var irmaoDoDinossauronaBlackFriday;
-
+var asfaltoquenaoeasfalto;
+var irmaodoasfalto;
 //Função para carregar os arquivos
 function preload(){
-  irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
-
+irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
+irmaodoasfalto = loadImage ("ground.png");
 }
-
 //Função de configuração
 function setup(){
-  //Cria a tela
-  createCanvas(600,200);
-  //Cria o sprite do dinossauro
-  dinossauroCorrendoNaBlackFriday = createSprite(50,160,20,50);
-  //Adiciona a animação ao dinossauro
-  dinossauroCorrendoNaBlackFriday.addAnimation("correndo", irmaoDoDinossauronaBlackFriday);
-  //Diminui ou aumenta a escala da variável
-  dinossauroCorrendoNaBlackFriday.scale = 0.5;
-  //Criando as bordas
-  borda = createEdgeSprites();
+//Cria a tela
+createCanvas(600,200);
+asfaltoquenaoeasfalto = createSprite (200,180,400,20);
+asfaltoquenaoeasfalto.addImage (irmaodoasfalto);
+asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2
+//Cria o sprite do dinossauro
+dinossauroCorrendoNaBlackFriday = createSprite(50,160,20,50);
+//Adiciona a animação ao dinossauro
+dinossauroCorrendoNaBlackFriday.addAnimation("correndo", irmaoDoDinossauronaBlackFriday);
+//Diminui ou aumenta a escala da variável
+dinossauroCorrendoNaBlackFriday.scale = 0.5;
+//Criando as bordas
+borda = createEdgeSprites();
 }
-
 //Função de desenho
 function draw(){
-  //Definindo a cor de fundo
-  background("white");
-
-  //Se apertar a tecla de espaço, o dinossauro pula
-  if(keyDown("space")){
-    dinossauroCorrendoNaBlackFriday.velocityY = -12;
-  }
-  //Sistema de gravidade
-  dinossauroCorrendoNaBlackFriday.velocityY += 1;
-
-  //Impede o dinossauro de cair da tela
-  dinossauroCorrendoNaBlackFriday.collide(borda[3]);
-  //Desenha todos os sprites
-  drawSprites();
+//Definindo a cor de fundo
+background("white");
+asfaltoquenaoeasfalto.velocityX = -2;
+if(asfaltoquenaoeasfalto.x<0) {
+asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
+}
+//Se apertar a tecla de espaço, o dinossauro pula
+if(keyDown("space")){
+dinossauroCorrendoNaBlackFriday.velocityY = -12;
+}
+//Sistema de gravidade
+dinossauroCorrendoNaBlackFriday.velocityY += 1;
+//Impede o dinossauro de cair da tela
+dinossauroCorrendoNaBlackFriday.collide(asfaltoquenaoeasfalto);
+//Desenha todos os sprites
+drawSprites();
 }
