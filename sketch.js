c7ac92300670ca3cbde012dd85f47653cc0a02f8
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-08-03 16:21:01 -0300

    03/08/2022

--- sketch.js
+++ sketch.js
@@ -1,45 +1,73 @@
-//Criando as variáveis
+//variáveis
 var dinossauroCorrendoNaBlackFriday;
 var irmaoDoDinossauronaBlackFriday;
 var asfaltoquenaoeasfalto;
 var irmaodoasfalto;
-//Função para carregar os arquivos
+var aviaodamulhermaravilha;
+
+//arquivos
 function preload(){
 irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
 irmaodoasfalto = loadImage ("ground.png");
 }
-//Função de configuração
+
+//configuração
 function setup(){
-//Cria a tela
+
+//tela
 createCanvas(600,200);
 asfaltoquenaoeasfalto = createSprite (200,180,400,20);
 asfaltoquenaoeasfalto.addImage (irmaodoasfalto);
-asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2
-//Cria o sprite do dinossauro
+asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
+aviaodamulhermaravilha = createSprite (50,190,400,10);
+aviaodamulhermaravilha.visible = false;
+
+//sprite do dinossauro
 dinossauroCorrendoNaBlackFriday = createSprite(50,160,20,50);
-//Adiciona a animação ao dinossauro
+
+//animação ao dinossauro
 dinossauroCorrendoNaBlackFriday.addAnimation("correndo", irmaoDoDinossauronaBlackFriday);
-//Diminui ou aumenta a escala da variável
+
+//escala da variável
 dinossauroCorrendoNaBlackFriday.scale = 0.5;
-//Criando as bordas
+
+//bordas
 borda = createEdgeSprites();
+
+//número aleatório
+var aleatorio = Math.round(random(1,100));
+console.log(aleatorio);
 }
-//Função de desenho
+
+//desenho
 function draw(){
-//Definindo a cor de fundo
+//console.log (dinossauroCorrendoNaBlackFriday.y);
+
+//cor de fundo
 background("white");
 asfaltoquenaoeasfalto.velocityX = -2;
 if(asfaltoquenaoeasfalto.x<0) {
 asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
 }
-//Se apertar a tecla de espaço, o dinossauro pula
-if(keyDown("space")){
+
+//tecla de espaço
+if(keyDown("space")&&dinossauroCorrendoNaBlackFriday.y>=150){
 dinossauroCorrendoNaBlackFriday.velocityY = -12;
 }
-//Sistema de gravidade
+
+//gravidade
 dinossauroCorrendoNaBlackFriday.velocityY += 1;
-//Impede o dinossauro de cair da tela
-dinossauroCorrendoNaBlackFriday.collide(asfaltoquenaoeasfalto);
+
+//dinossauro não cai da tela
+dinossauroCorrendoNaBlackFriday.collide(aviaodamulhermaravilha);
+
 //Desenha todos os sprites
 drawSprites();
+
+//Chamar a função que vai criar as nuvens
+oporcovoltou();
+}
+
+function oporcovoltou(){
+
 }
